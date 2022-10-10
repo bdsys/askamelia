1dbc62802a4ed8d968dfa8f13c86be3dbff10b28
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-10 23:33:51 +0000

    fixing date code and adding new intents

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Alexa app for the bb
 
 * Thanks to this article for help
     - https://medium.com/crowdbotics/how-to-build-a-custom-amazon-alexa-skill-step-by-step-my-favorite-chess-player-dcc0edae53fb
+
+* Checkout the Alexa beta
+	- https://skills-store.amazon.com/deeplink/tvt/fae75232cbfa4097303df85ecc596bf664e9c22dc15e7ec1a9f318f80ebed76ce185cb6ef0a22f1e7a540e4df84be133eacb1b175cbf83f84cbcc109976db0ef1c86b6845171dd211b49d137b664b9f924f3dbc743d4d14546cb4db102c0544970130614bdcae9a3cab586fa57778cdc
--- cdk/lambda/handler.py
+++ cdk/lambda/handler.py
@@ -19,7 +19,7 @@ def on_start():
     print("Session Started.")
 
 def on_launch(event):
-    onlunch_MSG = "Hi, I can tell you about Amelia Kat. Me and her are close friends."
+    onlunch_MSG = "Hi, I can tell you about Amelia Cat. Me and her are close friends."
     reprompt_MSG = "Try asking me how old Amelia is or what's her favorite kind of dog."
     # card_TEXT = "Pick a chess payer."
     # card_TITLE = "Choose a chess player."
@@ -45,6 +45,11 @@ def intent_scheme(event):
         return howManyMonths(birthdate_yyyymmdd)
     elif intent_name == "howManyWeeks":
         return howManyWeeks(birthdate_yyyymmdd)
+    elif intent_name == "favoriteColor":
+        return favoriteColor()
+    elif intent_name == "favoriteTypeOfDog":
+        return favoriteTypeOfDog()
+        
     elif intent_name in ["AMAZON.NoIntent", "AMAZON.StopIntent", "AMAZON.CancelIntent"]:
         return stop_the_skill(event)
     elif intent_name == "AMAZON.HelpIntent":
@@ -55,25 +60,34 @@ def intent_scheme(event):
 # Here we define the intent handler functions
 
 def howOldGeneral(birthdate_formatted):
-    reprompt_MSG = "Do you want to hear Amelia Kat's age again?"
-    card_TITLE = "You've asked about Amelia's age."
+    reprompt_MSG = "Do you want to hear Amelia Cat's age again?"
+    card_TITLE = "You've asked about Amelia's age. Ages less than 2 years will be reported in months."
     
     today_date = datetime.date.today()
     years_old = today_date.year - birthdate_formatted.year
-    month_difference = today_date.month - birthdate_formatted.month
+    month_difference = today_date.month - birthdate_formatted.month - 1
+    day_differene = birthdate_formatted.day - today_date.day
+    weeks_difference = round(day_differene/7)
+    days_remainder = day_differene%7
 
     # If date is more than 2 years, return years
     
     if(years_old > 2):
         
         if(month_difference == 0):
-            string_response = f"Amelia Kat is {years_old} years old exactly."
-            # card_TEXT = "Amelia Kat is {years_old} years old exactly."
+            string_response = f"Amelia Cat is {years_old} years old exactly."
+            # card_TEXT = "Amelia Cat is {years_old} years old exactly."
         else:
-            string_response = f"Amelia Kat is {years_old} year and {month_difference} months old."
+            string_response = f"Amelia Cat is {years_old} year and {month_difference} months old."
 
     elif(years_old == 0):
-        string_response = f"Amelia Kat is {month_difference} months old."
+        if(weeks_difference == 0):
+            string_response = f"Amelia Cat is {month_difference} months old exactly."
+        else:
+            string_response = f"Amelia Cat is {month_difference} months and {weeks_difference} old."
+
+    else:
+        string_response = f"Something went wrong."
 
     card_TEXT = string_response
 
@@ -83,29 +97,35 @@ def howOldGeneral(birthdate_formatted):
         False)
     
 def howManyMonths(birthdate_formatted):
-    reprompt_MSG = "Do you want to hear Amelia Kat's age in months again?"
+    reprompt_MSG = "Do you want to hear Amelia Cat's age in months again?"
     card_TITLE = "You've asked about Amelia's age in months."
     
     today_date = datetime.date.today()
     month_difference = today_date.month - birthdate_formatted.month
+    day_differene = birthdate_formatted.day - today_date.day
+    weeks_difference = round(day_differene/7)
+    days_remainder = day_differene%7
     
-    string_response = f"Amelia Kat is {month_difference} months old."
+    if(weeks_difference == 0):
+        string_response = f"Amelia Cat is {month_difference} months old exactly."
+    else:
+        string_response = f"Amelia Cat is {month_difference} months and {weeks_difference} old."
+    
     card_TEXT = string_response
-    
     print(f"Returning string: {string_response}")
     return output_json_builder_with_reprompt_and_card(
         string_response, card_TEXT, card_TITLE, reprompt_MSG, 
         False)
 
 def howManyWeeks(birthdate_formatted):
-    reprompt_MSG = "Do you want to hear Amelia Kat's age in weeks again?"
+    reprompt_MSG = "Do you want to hear Amelia Cat's age in weeks again?"
     card_TITLE = "You've asked about Amelia's age in weeks."
     card_TEXT = "You've asked about Amelia's age in weeks."
     
     today_date = datetime.date.today()
     years_old = today_date.year - birthdate_formatted.year
     month_difference = today_date.month - birthdate_formatted.month
-    day_difference = today_date.month - birthdate_formatted.month
+    day_difference = today_date.day - birthdate_formatted.day
     
     days_old_from_years = years_old * 365
     days_old_from_months = month_difference * 30
@@ -114,31 +134,59 @@ def howManyWeeks(birthdate_formatted):
     days_old = days_old_from_years + days_old_from_months + days_old_from_days
     weeks_old = round(days_old/7)
     
-    string_response = f"Amelia Kat is {weeks_old} weeks old."
+    string_response = f"Amelia Cat is {weeks_old} weeks old."
     card_TEXT = string_response
     
     print(f"Returning string: {string_response}")
     return output_json_builder_with_reprompt_and_card(
         string_response, card_TEXT, card_TITLE, reprompt_MSG, 
         False)
+        
+def favoriteColor():
+    reprompt_MSG = "Do you want to hear Amelia Cat's favorite color again?"
+    card_TITLE = "You've asked about Amelia's favorite color."
+    
+    # Dynamo code here
+    favorite_color = "red"
+    string_response = f"Amelia Cat's favorite color is {favorite_color}."
+    
+    card_TEXT = string_response
+    print(f"Returning string: {string_response}")
+    return output_json_builder_with_reprompt_and_card(
+        string_response, card_TEXT, card_TITLE, reprompt_MSG, 
+        False)
+        
+def favoriteTypeOfDog():
+    reprompt_MSG = "Do you want to hear Amelia Cat's favorite kind of dog again?"
+    card_TITLE = "You've asked about Amelia's favorite kind of dogr."
+    
+    # Dynamo code here
+    favorite_type_of_dog = "Corgi"
+    string_response = f"Amelia Cat's favorite kind of dog is a {favorite_type_of_dog}."
+    
+    card_TEXT = string_response
+    print(f"Returning string: {string_response}")
+    return output_json_builder_with_reprompt_and_card(
+        string_response, card_TEXT, card_TITLE, reprompt_MSG, 
+        False)
 
 def stop_the_skill(event):
-    stop_MSG = "Thank you. Bye!"
+    stop_MSG = "Check back again to see how much Amelia has grown. Goodbye!"
     reprompt_MSG = ""
-    card_TEXT = "Bye."
-    card_TITLE = "Bye Bye."
+    card_TEXT = "Bye! Check back again to see how much Amelia has grown!"
+    card_TITLE = "Bye!"
     return output_json_builder_with_reprompt_and_card(stop_MSG, card_TEXT, card_TITLE, reprompt_MSG, True)
     
 def assistance(event):
-    assistance_MSG = "Try asking me about Amelia Kat."
-    reprompt_MSG = "What do you want to know about Amelia Kat?"
+    assistance_MSG = "Try asking me about Amelia Cat."
+    reprompt_MSG = "What do you want to know about Amelia Cat?"
     card_TEXT = "You've asked for help."
     card_TITLE = "Help"
     return output_json_builder_with_reprompt_and_card(assistance_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
 
 def fallback_call(event):
     fallback_MSG = "I can't help you with that, try rephrasing the question or ask for help by saying HELP."
-    reprompt_MSG = "Do you want to know more about Amelia Kat?"
+    reprompt_MSG = "Do you want to know more about Amelia Cat?"
     card_TEXT = "You've asked a wrong question."
     card_TITLE = "Wrong question."
     return output_json_builder_with_reprompt_and_card(fallback_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
