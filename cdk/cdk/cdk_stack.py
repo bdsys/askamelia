7657a591d7c1206ab854307fedfe04439f07743c
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-13 22:30:10 +0000

    alexa skill programmatic

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -1,9 +1,13 @@
+# Stack deploys pre-requisite Alexa Skill resources
+
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     # aws_lambda_event_sources as lambda_trigger, # Alexa isn't supported yet
     # as an event source.
     aws_dynamodb as ddb,
+    aws_s3 as s3,
+    aws_iam as iam,
     RemovalPolicy,
 )
 from constructs import Construct
@@ -15,7 +19,7 @@ class CdkStack(Stack):
 
         # Defines an AWS Lambda resource
         _lambda.Function(
-            self, 'AskAmeliaHndler',
+            self, 'AskAmeliaHandler',
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda'),
             handler='handler.lambda_handler', # <cdk_folder>/lambda/handler.py
@@ -24,6 +28,25 @@ class CdkStack(Stack):
             }
         )
         
+        # Define IAM Roles
+        # Lambda execution role
+        ### TBD
+        
+        # Alexa Role
+        alexa_appkit_role = iam.Role(
+            self, "AskAmeliaS3AlexaRole",
+            assumed_by=iam.ServicePrincipal("alexa-appkit.amazon.com"),
+            description="IAM Role for Alexa appkit to access S3.",
+        )
+        
+        # Define S3 bucket
+        
+        skill_bucket = s3.Bucket(
+            self, "AskAmeliaSkillAssetsBucket",
+            encryption = s3.BucketEncryption.S3_MANAGED,
+            )
+        
+        
         # ddb.Table(
         #     self, 'AskAmeliaDynamoTableProperties',
         #     partition_key={'name': 'property', 'type': ddb.AttributeType.STRING},
@@ -31,3 +54,6 @@ class CdkStack(Stack):
         #     # # destroyed with a stack is deleted. This property will override
         #     # # CFN default and produce a destructive action.
         # )
+        
+        # Permissions
+        skill_bucket.grant_read(alexa_appkit_role)
