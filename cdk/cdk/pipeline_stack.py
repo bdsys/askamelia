5b5d2bbc26353791123ca152fa4bf4cbd849f6f6
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-21 23:02:34 +0000

    yuck

--- cdk/cdk/pipeline_stack.py
+++ cdk/cdk/pipeline_stack.py
@@ -31,7 +31,7 @@ class PipelineStack(Stack):
                     "cd cdk/", # move to CDK base
                     "ls", # list dir to show whats up
                     "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
-                    # "cdk --version", # Check on version of CDK for potential troubleshooting
+                    "cdk --version", # Check on version of CDK for potential troubleshooting
                     "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
                     "cdk synth", # Builds CDK app subject
                 ],
