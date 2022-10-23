b805ab6ec6bcc13e6cd119ffbd1255b6001d73f9
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-23 02:38:41 +0000

    shit

--- flask-auth-workshop/flask_auth_app/auth/project/__init__.py
+++ flask-auth-workshop/flask_auth_app/auth/project/__init__.py
@@ -1,27 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy(create_app())
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
 
-    db.init_app(app)
-    
-    from project import models
-    
-    db.create_all()
+from project import models
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+# db.init_app(app)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    
-    return app
+db.create_all()
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+
