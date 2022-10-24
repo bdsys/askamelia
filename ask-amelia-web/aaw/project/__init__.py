f8a1bd1b38c779d43d4a2b91e8129828c39054a7
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-24 03:23:38 +0000

    k

--- ask-amelia-web/aaw/project/__init__.py
+++ ask-amelia-web/aaw/project/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask # framework
 from flask_sqlalchemy import SQLAlchemy # ORM
 from flask_login import LoginManager # framework's session tracker
+from . import user_db
+from werkzeug.security import generate_password_hash
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -38,4 +40,37 @@ def create_app():
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    # If single user doesn't exist, create with env var password
+    
+    single_user_email = os.getenv('FLASK_AAW_USER_EMAIL')
+    
+    # Query User table for the first matching email, if any, as a dupe checker.
+    user = User.query.filter_by(email=single_user_email).first()
+    
+    if user: # if dupe checker is not None, redirect back to signup page for a retry
+    
+        print("Single user for the app doesn't exist, creating...")
+        print(f"Email: {os.getenv('FLASK_AAW_USER_EMAIL')}")
+        
+        single_user_password = os.getenv('FLASK_AAW_USER_PASSWORD')
+    
+        # Create object for query to User table to create a new row
+        new_user = User(
+            email=single_user_email, 
+            name="single-user",
+            password=generate_password_hash(
+                single_user_password,
+                method='sha256'
+            )
+        )
+        
+        # Perform query and commit using the new_user object
+        user_db.session.add(new_user)
+        user_db.session.commit()
+    
+    else:
+        
+        print(f"Single user exists -- {os.getenv('FLASK_AAW_USER_EMAIL')}")
+
     return app
