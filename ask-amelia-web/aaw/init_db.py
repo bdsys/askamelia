f1c1d472c9245aa4ddae8857b6a4ff24be5e045f
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-24 16:42:56 +0000

    k

--- ask-amelia-web/aaw/init_db.py
+++ ask-amelia-web/aaw/init_db.py
@@ -3,6 +3,8 @@ from project import db, create_app, models
 from project.models import User, AccessCodes
 from werkzeug.security import generate_password_hash
 
+print("Starting DB maintenance...")
+
 print("Creating SQLite database...")
 
 db.create_all(app=create_app()) # pass the create_app result so Flask-SQLAlchemy gets the configuration.
@@ -14,7 +16,6 @@ print("Seeding initial application dependencies...")
 app = create_app()
 
 print("Creating single user...")
-
 
 # If single user doesn't exist, create with env var password
 
@@ -48,3 +49,44 @@ with app.app_context():
         # Perform query and commit using the new_user object
         db.session.add(new_user)
         db.session.commit()
+        
+        print("Single user created!")
+        
+print("Finished with single user seed.")
+        
+print("Seeding access codes...")
+
+# Use SSM PS string list in the future...for now os.getenv
+
+# If access code doesn't exist, create
+
+access_code_envvar_1 = os.getenv('FLASK_AAW_ACESS_CODE_1')
+
+with app.app_context():
+    # Query User table for the first matching email, if any, as a dupe checker.
+    access_code_query = AccessCodes.query.filter_by(access_code=access_code_envvar_1).first()
+    
+    if access_code_query: # if dupe checker is not None, redirect back to signup page for a retry
+    
+        print(f"Access code exists -- {os.getenv('access_code_envvar_1')}")
+
+    else:
+
+        print("Access code for the app doesn't exist, creating...")
+        print(f"Access code: {os.getenv('access_code_envvar_1')}")
+        
+        # Create object for query to User table to create a new row
+        new_access_code = AccessCodes(
+            access_code=access_code_envvar_1,
+        )
+        
+        # Perform query and commit using the new_user object
+        db.session.add(new_access_code)
+        db.session.commit()
+        
+        print("New access code created!")
+print("Finished with access codes.")
+
+print("DB seeding is finished!")
+
+print("DB maintenance is finished!")
