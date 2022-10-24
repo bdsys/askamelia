031c8ef2678d07d048cc3dead84b6006bcf13682
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-24 16:28:35 +0000

    k

--- ask-amelia-web/aaw/project/models.py
+++ ask-amelia-web/aaw/project/models.py
@@ -7,3 +7,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    
+class AccessCodes(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    access_code = db.Column(db.String(100), unique=True)
