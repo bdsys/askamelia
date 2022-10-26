4882d22d0daf2c7411ed2d5aa046f9d8e3706a9b
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-26 01:06:45 +0000

    k

--- ask-amelia-web/aaw/project/auth.py
+++ ask-amelia-web/aaw/project/auth.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
@@ -34,6 +35,36 @@ def login_post():
     # redirect to protected view "main.profile"
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
+    
+# Login for all HTTP methods besides POST
+@auth.route('/invitation')
+def invitation():
+    return render_template('invitation.html')
+
+@auth.route('/invitation', methods=['POST'])
+def invitation_post():
+    
+    # Form input handling
+    email = os.getenv('FLASK_AAW_USER_EMAIL')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+    
+    # Query User table for first result of email matching email returned in form
+    user = User.query.filter_by(email=email).first()
+
+    # If the query doesn't return a user or the password is wrong
+    # These are combined to prevent a brute force email signups check
+    if not user or not check_password_hash(user.password, password):
+        flash('The invitation code you entered doesn''t seem right. Try again?')
+        return redirect(url_for('auth.invitation'))
+    
+    # If the query does return a user AND the password check passed from the above if,
+    # Vend a session to the client via signed cookie
+    ## and an additional signed cookie if the session is to be remembered via user input
+    # redirect to protected view "main.profile"
+    login_user(user, remember=remember)
+    return redirect(url_for('main.subject'))    
+
 
 # All HTTP methods besides POST
 @auth.route('/signup')
--- ask-amelia-web/aaw/project/main.py
+++ ask-amelia-web/aaw/project/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
+import requests, os
 from . import db
 
 main = Blueprint('main', __name__)
@@ -7,6 +8,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+    
+# @main.route('/')
+# def index():
+#     return redirect(url_for('auth.invitation'))
+    
 
 @main.route('/profile')
 @login_required # Decorator to protect route from unauthenticated users
@@ -14,3 +20,100 @@ def profile():
     # curent_user represents the ORM object, User in this case, that login_manager
     # used to authenticate the user via signed cookie on the client side
     return render_template('profile.html', name=current_user.name)
+    
+@main.route('/subject')
+@login_required # Decorator to protect route from unauthenticated users
+def subject():
+
+    # Code here to show a simple list of subjects from DDB table
+    # Singular right now.
+    
+    aa_api_get_db_item_by_pk_url = os.getenv('AA_API_GET_DB_ITEMS_BY_PK_URL')
+    
+    aa_ddb_pk_value = "amelia_cat"
+    
+    response_aa_api_get_db_item_by_pk = requests.get(aa_api_get_db_item_by_pk_url)
+    
+    response_json_dict = response_aa_api_get_db_item_by_pk.json()
+    
+    render_subject = response_json_dict['subject']
+    
+    return render_template(
+        'subject.html',
+        render_subject=render_subject,
+    )
+
+@main.route('/update')
+@login_required # Decorator to protect route from unauthenticated users
+def update():
+    
+    # Code here to render the DDB table and provide updatable fields and update button
+    
+    aa_api_get_db_item_by_pk_url = os.getenv('AA_API_GET_DB_ITEMS_BY_PK_URL')
+    aa_api_update_ddb_item_by_pk_url = os.getenv('AA_API_UPDATE_DDB_ITEM_BY_PK_URL')
+    
+    aa_ddb_pk_value = "amelia_cat"
+    
+    response_aa_api_get_db_item_by_pk = requests.get(aa_api_get_db_item_by_pk_url)
+    
+    response_json_dict = response_aa_api_get_db_item_by_pk.json()
+    
+    render_subject = response_json_dict['subject']
+    render_birth_date = response_json_dict['birth_date']
+    render_favorite_color = response_json_dict['favorite_color']
+    render_test_value = response_json_dict['test_value']
+    render_favorite_dog_breed = response_json_dict['favorite_dog_breed']
+    
+    return render_template(
+        'update.html',
+        render_subject=render_subject,
+        render_birth_date = render_birth_date,
+        render_favorite_color = render_favorite_color,
+        render_test_value = render_test_value,
+        render_favorite_dog_breed = render_favorite_dog_breed,
+    )
+    
+@main.route('/update', methods=['POST'])
+@login_required # Decorator to protect route from unauthenticated users
+def update_post():
+
+    # Form input handling
+    form_input_subject = request.form.get('subject')
+    form_input_birth_date = request.form.get('password')
+    form_input_favorite_color = request.form.get('favcolor')
+    form_input_test_value = request.form.get('testvalue')
+    form_input_favorite_dog_breed = request.form.get('favdogbreed')
+    
+    aa_api_update_ddb_item_by_pk_url = os.getenv('AA_API_UPDATE_DDB_ITEM_BY_PK_URL')
+    
+    aa_ddb_update_dict = { 
+        "subject": form_input_subject,
+        'birth_date': form_input_birth_date,
+        'favorite_color': form_input_favorite_color,
+        'test_value': form_input_test_value,
+        'favorite_dog_breed': form_input_favorite_dog_breed,
+    }
+    
+    response_aa_api_update_ddb_item_by_pk = requests.post(
+        aa_api_update_ddb_item_by_pk_url, 
+        json=aa_ddb_update_dict,
+    )
+    
+    
+    response_json_dict = response_aa_api_update_ddb_item_by_pk.json()
+    response_json_dict_ddb_updated = response_json_dict['operation_message']
+    
+    render_subject = response_json_dict_ddb_updated['subject']
+    render_birth_date = response_json_dict_ddb_updated['birth_date']
+    render_favorite_color = response_json_dict_ddb_updated['favorite_color']
+    render_test_value = response_json_dict_ddb_updated['test_value']
+    render_favorite_dog_breed = response_json_dict_ddb_updated['favorite_dog_breed']
+    
+    return render_template(
+        'update.html',
+        render_subject=render_subject,
+        render_birth_date = render_birth_date,
+        render_favorite_color = render_favorite_color,
+        render_test_value = render_test_value,
+        render_favorite_dog_breed = render_favorite_dog_breed,
+    )
