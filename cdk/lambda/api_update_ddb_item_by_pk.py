e5b5a1623bc6d0caa525557d32771e748e0af645
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-27 02:19:39 +0000

    k

--- cdk/lambda/api_update_ddb_item_by_pk.py
+++ cdk/lambda/api_update_ddb_item_by_pk.py
@@ -2,96 +2,89 @@
 ## Needs input validations!
 
 import json, boto3, os
+from boto3.dynamodb.conditions import Key, Attr
 
-ddb_resource = boto3.resource('dynamodb')  
+ddb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    
-    print (f"event: {event}")
-    print (f"context {context}")
-
     try:
-    
-        print(f"Event PK: {event['pk']}")
-        print(f"Event pk_value: {event['pk_value']}")
-        print(f"Event update_key: {event['update_key']}")
-        print(f"Event update_value: {event['update_value']}")
+        print (f"event: {event}")
+        print (f"context {context}")
+        
+        try:
+        
+            print(f"Event PK: {event['body']}")
+            
+            pk_arg = json.loads(event['body'])
+            pk_arg_object = pk_arg['pk']
 
+        except Exception:
+            print(Exception)
+            http_status_code_return = 400
+            http_body_return = json.dumps("Bad request")
+            
+            return {
+                'statusCode': http_status_code_return,
+                'body': http_body_return
+            }
+        
+        print("Calling function get_table_items_by_pk")
+            
+        get_items_response = get_table_items_by_pk(
+            os.environ['ask_amelia_property_ddb_table'],
+            os.environ["ask_amelia_primary_key_static"],
+            # os.environ["ask_amelia_primary_key_value_static"],
+            pk_arg_object,
+        )
+        
+        print(f"Items list sent back from get_table_items_by_pk: {get_items_response[0]}")
+        print(f"count of items in list get_items_response: {len(get_items_response[0])}")
+        
+        print("Cleaning up ddb domain specific JSON return.")
+        formatted_list_return = dict()
+        item_number = 1
+        for item in get_items_response[0]:
+            print(f"item {item_number}: {item} -- {get_items_response[0][item]['S']}")
+            formatted_list_return[item] = get_items_response[0][item]['S']
+            item_number += 1
+        print(f"Cleaned dictonary (flat) to be returned to requester: {formatted_list_return}")
+        
+        print(f"Cleaned dictonary (iterated) to be returned to requester: {formatted_list_return}")
+        item_number = 1
+        for item in formatted_list_return:
+            print(f"item {item_number}: {item} -- {formatted_list_return[item]}")
+            item_number += 1
+            
+        http_status_code_return = 200
+        http_body_return = json.dumps(formatted_list_return)
+        
     except Exception:
         print(Exception)
-        http_status_code_return = 400
-        http_body_return = json.dumps("Bad request")
-        
-        return {
-            'statusCode': http_status_code_return,
-            'body': http_body_return
-        }
+        http_status_code_return = 500
+        http_body_return = json.dumps("Unknown error has occured!")
 
-
-    print("Calling function update_table_item_by_pk")
-    
-    # Function takes a single key and value to perform an upate
-    
-    # # Test case -- DISABLE ME ONCE THE FULL IMPLEMENTATION IS TESTED
-    # update_table_return = update_table_item_by_pk(
-    #     os.environ['ask_amelia_property_ddb_table'],
-    #     os.environ["ask_amelia_primary_key_static"],
-    #     os.environ["ask_amelia_primary_key_value_static"],
-    #     "test_value",
-    #     "69"
-    # )
-
-
-    update_table_return = update_table_item_by_pk(
-        os.environ['ask_amelia_property_ddb_table'],
-        event['pk'],
-        event['pk_value']
-        event['update_key'],
-        event['update_value'],
-        )
-
-    print(f"Update function return: {update_table_return}")
-
-    formatted_return = dict()
-    
-    formatted_return["status"] = "success"
-    formatted_return["operation_message"] = update_table_return["Attributes"]
-    
-    http_status_code_return = 200
-    http_body_return = json.dumps(formatted_return)
-        
     return {
         'statusCode': http_status_code_return,
         'body': http_body_return
     }
 
 
-def update_table_item_by_pk(table, pk_1, pk_value, update_key, update_value):
+def get_table_items_by_pk(table, pk_1, pk_value):
     
-    print("Invoked function update_table_item_by_pk")
+    print("Invoked function get_table_items_by_pk")
     print("Passed args:")
     print(f"table:{table}")
-    print(f"pk_1:{pk_1}")
     print(f"pk_value:{pk_value}")
-    print(f"update_key: {update_key}")
-    print(f"update_value: {update_value}")
-    
-    table = ddb_resource.Table(table)
-    
-    update_response = table.update_item(
-        Key={
-            f'{pk_1}': pk_value,
-        },
-        UpdateExpression=f"set {update_key} = :u_k",
+
+    query_response = ddb_client.query(
+        TableName=table,
+        KeyConditionExpression=f'{pk_1} = :{pk_1}',
         ExpressionAttributeValues={
-            ':u_k': update_value
-        },
-        ReturnValues="ALL_NEW"
-        # ALL_NEW - Returns all of the attributes of the item, as they appear after the UpdateItem operation.
-        # UPDATED_NEW - Returns only the updated attributes, as they appear after the UpdateItem operation.
+            f':{pk_1}': {'S': pk_value}
+        }
     )
     
+    print (f"response return will be: {query_response}")
+    print(f"formatted response: {query_response['Items']}")
     
-    print (f"response return will be: {update_response}")
-    
-    return update_response
+    return query_response['Items']
