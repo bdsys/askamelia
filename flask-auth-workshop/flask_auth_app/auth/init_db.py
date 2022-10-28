7b76d0fa6c6a3e57df5f6631e38dc37d13d462b8
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-28 19:00:35 +0000

    k

--- flask-auth-workshop/flask_auth_app/auth/init_db.py
+++
@@ -1,7 +0,0 @@
-from project import db, create_app, models
-
-print("Creating SQLite database...")
-
-db.create_all(app=create_app()) # pass the create_app result so Flask-SQLAlchemy gets the configuration.
-
-print("Created!")
--- flask-auth-workshop/flask_auth_app/auth/project/models.py
+++
@@ -1,9 +0,0 @@
-from flask_login import UserMixin
-from . import db
-
-# UserMixin "mixes in" flask_login attributes to the User row in the table to integrate into the class
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
