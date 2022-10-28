3fe87b96f206d16a62258e3e0ff73a93f6cb2f17
EC2 Default User <ec2-user@ip-172-31-18-170.us-west-2.compute.internal>
2022-10-28 18:37:19 +0000

    k

--- ask-amelia-web/aaw/start-flask.sh
+++ ask-amelia-web/aaw/start-flask.sh
@@ -2,7 +2,7 @@
 echo "Starting http://dev.askamelia.bdsys.net:5000 on Flask as background process with nohup..."
 nohup flask run --host=0.0.0.0 --port=5000 &
 echo "Started!"
-echo "Starting http://askamelia.bdsys.net:80 on Flask as background process with nohup..."
-nohup flask run --host=0.0.0.0 --port=80 &
-echo "Started!"
+# echo "Starting http://askamelia.bdsys.net:80 on Flask as background process with nohup..."
+# nohup flask run --host=0.0.0.0 --port=80 &
+# echo "Started!"
 echo "Done!"
